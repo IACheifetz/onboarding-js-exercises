9cef89942cee362ab3a17877ed750745329a812b
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-02-28 15:28:59 -0800

    areaOfTriangle completed

--- app.js
+++ app.js
@@ -16,8 +16,12 @@ console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 // 2. areaOfTriangle
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
-function areaOfTriangle() {}
-
+function areaOfTriangle(area, base, height) {
+    base = ( 5 );
+    height = ( 10 );
+    area = ( base * height ) / 2;
+    return area;
+}
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
 // 3. lessThan100
